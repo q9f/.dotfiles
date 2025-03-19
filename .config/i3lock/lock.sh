8fd3c049adb48070a09103e828fed3d89a9691de
Afri Schoedon <58883403+q9f@users.noreply.github.com>
2025-03-19 18:00:30 +0100

    switch to i3lock-color

--- .config/i3lock/lock.sh
+++ .config/i3lock/lock.sh
@@ -1,9 +1,46 @@
 #!/bin/bash
 
 setxkbmap -layout us -option compose:ralt
-PICTURE=$HOME/.config/i3lock/i3lock.png
-scrot $PICTURE
-convert $PICTURE -blur "5x4" $PICTURE
-i3lock -i $PICTURE
-rm $PICTURE
-sleep 1
+
+ALPHA='dd'
+SELECTION='#44475a'
+ORANGE='#ffb86c'
+RED='#ff5555'
+MAGENTA='#ff79c6'
+BLUE='#6272a4'
+GREEN='50fa7b'
+
+i3lock \
+  --insidever-color=$SELECTION$ALPHA \
+  --insidewrong-color=$SELECTION$ALPHA \
+  --inside-color=$SELECTION$ALPHA \
+  --ringver-color=$GREEN$ALPHA \
+  --ringwrong-color=$RED$ALPHA \
+  --ring-color=$BLUE$ALPHA \
+  --line-uses-ring \
+  --keyhl-color=$MAGENTA$ALPHA \
+  --bshl-color=$ORANGE$ALPHA \
+  --separator-color=$SELECTION$ALPHA \
+  --verif-color=$GREEN \
+  --wrong-color=$RED \
+  --modif-color=$RED \
+  --layout-color=$BLUE \
+  --date-color=$BLUE \
+  --time-color=$BLUE \
+  --screen 1 \
+  --blur 3 \
+  --clock \
+  --indicator \
+  --time-str="%H:%M:%S" \
+  --date-str="%e.%m.%Y" \
+  --verif-text="Checking..." \
+  --wrong-text="Wrong" \
+  --noinput="No input" \
+  --lock-text="Locking..." \
+  --lockfailed="Lock failed" \
+  --radius=120 \
+  --ring-width=10 \
+  --pass-media-keys \
+  --pass-screen-keys \
+  --pass-volume-keys
+
